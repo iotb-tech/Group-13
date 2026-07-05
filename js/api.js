9d2a353557daf00fa183d58fcbcc077c45027872
EhmbKay Codes <olawalemubarak26@gmail.com>
2026-07-05 14:59:51 +0100

    api and search logic

---
+++ js/api.js
@@ -0,0 +1,32 @@
+const BASE_URL = "https://api.jikan.moe/v4/anime";
+
+/**
+ * Fetch anime from Jikan API
+ *
+ * @param {string} query
+ * @returns {Promise<Array>}
+ */
+async function fetchAnime(query) {
+
+    try {
+        const url =
+            ${BASE_URL}?q=${encodeURIComponent(query)}&limit=12;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Unable to fetch anime.");
+        }
+
+        const result = await response.json();
+
+        if (!result.data) {
+            return [];
+        }
+        return result.data;
+    }
+
+    catch (error) {
+        console.error("API Error:", error);
+        throw error;
+    }
+
+}
